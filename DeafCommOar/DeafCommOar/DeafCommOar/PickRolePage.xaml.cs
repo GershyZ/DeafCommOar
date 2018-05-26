0be3fe84b5d965924591342c88f70aff7eea5a62
Gershon Zebovitz <zebovi40@gmail.com>
2018-05-26 12:52:57 -0400

    Pick Role Page

--- DeafCommOar/DeafCommOar/DeafCommOar/PickRolePage.xaml.cs
+++ DeafCommOar/DeafCommOar/DeafCommOar/PickRolePage.xaml.cs
@@ -1,5 +1,4 @@
-﻿using DeafCommOar.ViewModels;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,42 +9,17 @@ using Xamarin.Forms.Xaml;
 
 namespace DeafCommOar
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class PickRolePage : ContentPage
-	{
-        private PickRolePageVM _PickRoleVM = new PickRolePageVM();         
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PickRolePage : TabbedPage
+    {
         public PickRolePage ()
-		{
-			InitializeComponent ();
-            BindingContext = _PickRoleVM;            
-		}
-
-        private void Picker_SelectedIndexChanged(object sender, EventArgs e)
         {
-
-            sl_coxswain_controls.IsVisible = false;
-            sl_athlete_form.IsVisible = false;
-            String role = _PickRoleVM.getRole(((Picker)sender).SelectedIndex);
-            if(role.ToCharArray()[0] == 'C')
-            {
-                sl_coxswain_controls.IsVisible = true;
-            }
-            if(role.ToCharArray()[0] == 'R') {
-                sl_athlete_form.IsVisible = true;
-            }
+            InitializeComponent();
         }
 
-        private void Button_Clicked(object sender, EventArgs e)
+        public void CreateTeamHotspot(object s, EventArgs e)
         {
-            String role = _PickRoleVM.getRole(((Picker)sender).SelectedIndex);
-            if (role.ToCharArray()[0] == 'C')
-            {
-                App.Current.MainPage = new Pages.ConfigurationPage();
-            }
-            if (role.ToCharArray()[0] == 'R')
-            {
-                App.Current.MainPage = new Pages.CommandReceiverPage();
-            }
+            App.Current.MainPage = new Pages.ConfigurationPage();
         }
     }
 }
